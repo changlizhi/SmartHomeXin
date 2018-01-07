4d3936ab0228e7380dd6085a3b504d197d57cc5f
changlz <changlizhi@aliyun.com>
2018-01-07 21:13:09 +0800

    重写调用方式

--- zifuchuan/zifuchuan.h
+++ zifuchuan/zifuchuan.h
@@ -1,9 +1,9 @@
-extern void replaceFirst(char *str1,char *str2,char *str3);
-extern void replace(char *str1,char *str2,char *str3);
-extern void substring(char *dest,char *src,int start,int end);
-extern char charAt(char *src,int index);
-extern int indexOf(char *str1,char *str2);
-extern int lastIndexOf(char *str1,char *str2);
-extern void ltrim(char *str);
-extern void rtrim(char *str);
-extern void trim(char *str);
+void replaceFirst(char *str1,char *str2,char *str3);
+void replace(char *str1,char *str2,char *str3);
+void substring(char *dest,char *src,int start,int end);
+char charAt(char *src,int index);
+int indexOf(char *str1,char *str2);
+int lastIndexOf(char *str1,char *str2);
+void ltrim(char *str);
+void rtrim(char *str);
+void trim(char *str);
