455392a6f31a9242e84c86347c229db5eb9b4440
changlz <changlizhi@aliyun.com>
2018-01-07 21:33:18 +0800

    不在外部引用了

--- zifuchuan/zifuchuan.h
+++
@@ -1,9 +0,0 @@
-void replaceFirst(char *str1,char *str2,char *str3);
-void replace(char *str1,char *str2,char *str3);
-void substring(char *dest,char *src,int start,int end);
-char charAt(char *src,int index);
-int indexOf(char *str1,char *str2);
-int lastIndexOf(char *str1,char *str2);
-void ltrim(char *str);
-void rtrim(char *str);
-void trim(char *str);
