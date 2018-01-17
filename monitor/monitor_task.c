d7fa163767d4a3cb9ec3dba17011ea0080187785
changlz <changlizhi@aliyun.com>
2018-01-17 15:39:09 +0800

    恢复播放时间

--- monitor/monitor_task.c
+++ monitor/monitor_task.c
@@ -882,30 +882,29 @@ static void trans(long sec){
 }
 
 static void *Yinpinguoqi(void *arg){
-//    char *lujing = "/tmp/mounts/SD-P1/play/shock.mp3";
+    char *lujing = "/tmp/mounts/SD-P1/play/shock.mp3";
     while(1){
-//        PrintLog(0,"lujing-----%s\n",lujing);
-//        time_t t;
-//        t=time(0);//当前时间秒数
-//
-//        PrintLog(0,"dangqian shijian-----%ld\n",t);
-//        trans(t);
-//        struct stat buf;
-//        int result;
-//        //获得文件状态信息
-//        result =stat(lujing, &buf );
-//        //显示文件状态信息
-//        if( result != 0 ){
-//            PrintLog(0,"wenjian chucuo" );//并提示出错的原因，如No such file or directory（无此文件或索引）
-//        }
-//        else
-//        {
-//            PrintLog(0,"wenjian daxiao: %d\n", buf.st_size);
-//            PrintLog(0,"chuangjian shijian : %s\n", ctime(&buf.st_ctime));
-//            PrintLog(0,"fangwen shijian : %s\n", ctime(&buf.st_atime));
-//            PrintLog(0,"xiugai shijian: %s\n", ctime(&buf.st_mtime));
-//        }
-        PrintLog(0,"1111111111111111111");
+        PrintLog(0,"lujing-----%s\n",lujing);
+        time_t t;
+        t=time(0);//当前时间秒数
+
+        PrintLog(0,"dangqian shijian-----%ld\n",t);
+        trans(t);
+        struct stat buf;
+        int result;
+        //获得文件状态信息
+        result =stat(lujing, &buf );
+        //显示文件状态信息
+        if( result != 0 ){
+            PrintLog(0,"wenjian chucuo" );//并提示出错的原因，如No such file or directory（无此文件或索引）
+        }
+        else
+        {
+            PrintLog(0,"wenjian daxiao: %d\n", buf.st_size);
+            PrintLog(0,"chuangjian shijian : %s\n", ctime(&buf.st_ctime));
+            PrintLog(0,"fangwen shijian : %s\n", ctime(&buf.st_atime));
+            PrintLog(0,"xiugai shijian: %s\n", ctime(&buf.st_mtime));
+        }
         Sleep(600);
     }
 }
